9046aeac67013af73ffb3e696921edd9f5ff7c5f
Raiden <raidenaz74@gmail.com>
2023-12-28 00:32:40 -0700

    Fixing sudo issues

--- rebuild.nix
+++ rebuild.nix
@@ -1,7 +1,7 @@
-{writeShellApplication, nixos-rebuild, git, sudo}: writeShellApplication {
+{writeShellApplication, nixos-rebuild, git}: writeShellApplication {
 name = "rebuild";
 
-runtimeInputs = [ nixos-rebuild git sudo];
+runtimeInputs = [ nixos-rebuild git];
 
 text = "sudo nixos-rebuild switch --flake github:StrawberryJam2/NIXOS";
 }
